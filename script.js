3ac093aba25ada999e39b730892689b1d93e5d1a
ThaiGiang08 <96651615+ThaiGiang08@users.noreply.github.com>
2022-04-14 11:21:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var d = new Date().getDay();
 if (d!=0) {
-	var days = ['Thứ hai','Thứ ba','Thứ tư','Thứ năm','Thứ sáu','Thứ bảy'];
+	var days = ['THỨ HAI','THỨ BA','THỨ TƯ','THỨ NĂM','THỨ SÁU','THỨ BẢY','CHỦ NHẬT'];
 	var day = days[d-1];
 	var inputText = document.getElementById(day);
 	var index = 7;
