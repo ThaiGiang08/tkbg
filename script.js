5f66e5ce748b6fd555c719df25cb537152278571
ThaiGiang08 <96651615+ThaiGiang08@users.noreply.github.com>
2022-04-16 06:29:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var d = new Date().getDay();
 if (d!=0) {
-	var days = ['THỨ HAI','THỨ BA','THỨ TƯ','THỨ NĂM','THỨ SÁU','THỨ BẢY','CHỦ NHẬT'];
+	var days = ['HAI','BA','TƯ','NĂM','SÁU','BẢY','CHỦ NHẬT'];
 	var day = days[d-1];
 	var inputText = document.getElementById(day);
-	var index = 7;
+	var index = 8
 	if (index >= 0) {
 		innerHTML = "<mark>" + day + " </mark>";
 		inputText.innerHTML = innerHTML;
